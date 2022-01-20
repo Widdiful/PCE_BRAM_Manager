7b8ff97d7aa4554db30f58e5b443de3218612ca1
Widdiful <Widdiful@users.noreply.github.com>
2022-01-20 22:43:15 +0000

    Working prototype
    Load two BRAM files
    Select and delete save data
    Copy save data
    Reorder list
    Save BRAM file

---
+++ BRAM_Manager/MainForm.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace BRAM_Manager {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LeftList = new System.Windows.Forms.ListBox();
+            this.RightList = new System.Windows.Forms.ListBox();
+            this.LeftAddress = new System.Windows.Forms.TextBox();
+            this.RightAddress = new System.Windows.Forms.TextBox();
+            this.LeftBrowse = new System.Windows.Forms.Button();
+            this.RightBrowse = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.LeftSave = new System.Windows.Forms.Button();
+            this.RightSave = new System.Windows.Forms.Button();
+            this.LeftFreeSpace = new System.Windows.Forms.Label();
+            this.RightFreeSpace = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(370, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // LeftList
+            // 
+            this.LeftList.FormattingEnabled = true;
+            this.LeftList.Location = new System.Drawing.Point(12, 27);
+            this.LeftList.Name = "LeftList";
+            this.LeftList.Size = new System.Drawing.Size(120, 199);
+            this.LeftList.TabIndex = 1;
+            this.LeftList.SelectedIndexChanged += new System.EventHandler(this.LeftList_SelectedIndexChanged);
+            // 
+            // RightList
+            // 
+            this.RightList.FormattingEnabled = true;
+            this.RightList.Location = new System.Drawing.Point(238, 27);
+            this.RightList.Name = "RightList";
+            this.RightList.Size = new System.Drawing.Size(120, 199);
+            this.RightList.TabIndex = 2;
+            this.RightList.SelectedIndexChanged += new System.EventHandler(this.RightList_SelectedIndexChanged);
+            // 
+            // LeftAddress
+            // 
+            this.LeftAddress.Location = new System.Drawing.Point(12, 232);
+            this.LeftAddress.Name = "LeftAddress";
+            this.LeftAddress.ReadOnly = true;
+            this.LeftAddress.Size = new System.Drawing.Size(120, 20);
+            this.LeftAddress.TabIndex = 3;
+            // 
+            // RightAddress
+            // 
+            this.RightAddress.Location = new System.Drawing.Point(238, 232);
+            this.RightAddress.Name = "RightAddress";
+            this.RightAddress.ReadOnly = true;
+            this.RightAddress.Size = new System.Drawing.Size(120, 20);
+            this.RightAddress.TabIndex = 4;
+            // 
+            // LeftBrowse
+            // 
+            this.LeftBrowse.Location = new System.Drawing.Point(75, 259);
+            this.LeftBrowse.Name = "LeftBrowse";
+            this.LeftBrowse.Size = new System.Drawing.Size(56, 23);
+            this.LeftBrowse.TabIndex = 5;
+            this.LeftBrowse.Text = "Browse...";
+            this.LeftBrowse.UseVisualStyleBackColor = true;
+            this.LeftBrowse.Click += new System.EventHandler(this.LeftBrowse_Click);
+            // 
+            // RightBrowse
+            // 
+            this.RightBrowse.Location = new System.Drawing.Point(302, 258);
+            this.RightBrowse.Name = "RightBrowse";
+            this.RightBrowse.Size = new System.Drawing.Size(56, 23);
+            this.RightBrowse.TabIndex = 6;
+            this.RightBrowse.Text = "Browse...";
+            this.RightBrowse.UseVisualStyleBackColor = true;
+            this.RightBrowse.Click += new System.EventHandler(this.RightBrowse_Click);
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(148, 161);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 7;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(148, 190);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(148, 132);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(75, 23);
+            this.DownButton.TabIndex = 9;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // UpButton
+            // 
+            this.UpButton.Location = new System.Drawing.Point(148, 103);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(75, 23);
+            this.UpButton.TabIndex = 10;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // LeftSave
+            // 
+            this.LeftSave.Location = new System.Drawing.Point(12, 259);
+            this.LeftSave.Name = "LeftSave";
+            this.LeftSave.Size = new System.Drawing.Size(56, 23);
+            this.LeftSave.TabIndex = 11;
+            this.LeftSave.Text = "Save";
+            this.LeftSave.UseVisualStyleBackColor = true;
+            this.LeftSave.Click += new System.EventHandler(this.LeftSave_Click);
+            // 
+            // RightSave
+            // 
+            this.RightSave.Location = new System.Drawing.Point(238, 258);
+            this.RightSave.Name = "RightSave";
+            this.RightSave.Size = new System.Drawing.Size(56, 23);
+            this.RightSave.TabIndex = 12;
+            this.RightSave.Text = "Save";
+            this.RightSave.UseVisualStyleBackColor = true;
+            this.RightSave.Click += new System.EventHandler(this.RightSave_Click);
+            // 
+            // LeftFreeSpace
+            // 
+            this.LeftFreeSpace.AutoSize = true;
+            this.LeftFreeSpace.Location = new System.Drawing.Point(9, 285);
+            this.LeftFreeSpace.Name = "LeftFreeSpace";
+            this.LeftFreeSpace.Size = new System.Drawing.Size(37, 13);
+            this.LeftFreeSpace.TabIndex = 13;
+            this.LeftFreeSpace.Text = "          ";
+            // 
+            // RightFreeSpace
+            // 
+            this.RightFreeSpace.AutoSize = true;
+            this.RightFreeSpace.Location = new System.Drawing.Point(235, 285);
+            this.RightFreeSpace.Name = "RightFreeSpace";
+            this.RightFreeSpace.Size = new System.Drawing.Size(37, 13);
+            this.RightFreeSpace.TabIndex = 14;
+            this.RightFreeSpace.Text = "          ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 306);
+            this.Controls.Add(this.RightFreeSpace);
+            this.Controls.Add(this.LeftFreeSpace);
+            this.Controls.Add(this.RightSave);
+            this.Controls.Add(this.LeftSave);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.RightBrowse);
+            this.Controls.Add(this.LeftBrowse);
+            this.Controls.Add(this.RightAddress);
+            this.Controls.Add(this.LeftAddress);
+            this.Controls.Add(this.RightList);
+            this.Controls.Add(this.LeftList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ListBox LeftList;
+        private System.Windows.Forms.ListBox RightList;
+        private System.Windows.Forms.TextBox LeftAddress;
+        private System.Windows.Forms.TextBox RightAddress;
+        private System.Windows.Forms.Button LeftBrowse;
+        private System.Windows.Forms.Button RightBrowse;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button LeftSave;
+        private System.Windows.Forms.Button RightSave;
+        private System.Windows.Forms.Label LeftFreeSpace;
+        private System.Windows.Forms.Label RightFreeSpace;
+    }
+}
+
---
+++ BRAM_Manager/MainForm.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BRAM_Manager {
+    public partial class MainForm : Form {
+        public string initialDirectory = "c:\\";
+        public BRAM leftBRAM, rightBRAM;
+
+        public struct BRAM {
+            public byte[] data;
+            public byte[] header;
+            public List<BRAMEntry> saves;
+            public int nextSlot;
+            public int freeSpace;
+            public bool loaded;
+        }
+
+        public struct BRAMEntry {
+            public byte[] data;
+            public string name;
+            public int length;
+            public int startsAt;
+        }
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        public string OpenFileBrowser() {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = initialDirectory;
+                openFileDialog.Filter = "BRAM files (*.bup)|*.bup|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    //Get the path of specified file
+                    initialDirectory = Path.GetDirectoryName(openFileDialog.FileName);
+                    return openFileDialog.FileName;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public byte[] ReadBytes(byte[] data, int from, int to) {
+            byte[] result = new byte[to - from];
+
+            int index = 0;
+            for (int i = from; i < to; i++) {
+                result[index] = data[i];
+                index++;
+            }
+
+            return result;
+        }
+
+        public void WriteBytes(byte[] input, byte[] dest, int startLoc, int length) {
+            int index = 0;
+            for (int i = startLoc; i < startLoc + length; i++) {
+                dest[i] = input[index];
+                index++;
+            }
+        }
+
+        public BRAM ReadFile(string file) {
+            BRAM result = new BRAM();
+            result.saves = new List<BRAMEntry>();
+
+            string dataString = System.IO.File.ReadAllText(file);
+            byte[] data = System.IO.File.ReadAllBytes(file);
+            if (dataString.StartsWith("HUBM")) {
+                int length = data.Length;
+                result.data = data;
+                result.header = ReadBytes(data, 0, 16);
+
+                // bytes 7 and 8 are pointers to the next available memory slot
+                byte[] nextSlotBytes = ReadBytes(data, 6, 8);
+                result.nextSlot = BitConverter.ToUInt16(nextSlotBytes, 0);
+                result.nextSlot -= 32768; // remove the offset of 0x8000 to get the number we actually want
+                result.freeSpace = 2048 - result.nextSlot;
+
+                int nextSaveIndex = 16;
+                int nextLength = BitConverter.ToInt16(data, nextSaveIndex);
+
+                // loop through saves until reaching a file that is 0 bytes long (no file)
+                while (nextLength != 0) {
+                    BRAMEntry newEntry = new BRAMEntry();
+
+                    newEntry.length = nextLength;
+                    newEntry.data = ReadBytes(data, nextSaveIndex, nextSaveIndex + nextLength);
+                    newEntry.name = System.Text.Encoding.UTF8.GetString(ReadBytes(data, nextSaveIndex + 6, nextSaveIndex + 16));
+                    newEntry.startsAt = nextSaveIndex;
+
+                    result.saves.Add(newEntry);
+                    nextSaveIndex += nextLength;
+                    nextLength = BitConverter.ToInt16(data, nextSaveIndex);
+                }
+
+                result.loaded = true;
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("File is not a valid BRAM file.");
+            }
+
+            return result;
+        }
+
+        public void SaveFile(BRAM bram, string path) {
+            byte[] outputData = new byte[2048];
+
+            // create new data array
+            WriteBytes(bram.header, outputData, 0, 16);
+            int index = 16;
+            foreach(BRAMEntry entry in bram.saves) {
+                WriteBytes(entry.data, outputData, index, entry.length);
+                index += entry.length;
+            }
+
+            // calculate new next slot pointer
+            int nextSlot = index;
+            byte[] nextSlotBytes = new byte[2];
+            nextSlot += 32768;
+            nextSlotBytes = BitConverter.GetBytes(nextSlot);
+            WriteBytes(nextSlotBytes, outputData, 6, 2);
+
+            File.WriteAllBytes(path, outputData);
+        }
+
+        public void SwapListItems<T>(List<T> list, int indexA, int indexB) {
+            T temp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = temp;
+        }
+
+        public void SwapListItems(ListBox.ObjectCollection list, int indexA, int indexB) {
+            object temp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = temp;
+        }
+
+        private void LeftBrowse_Click(object sender, EventArgs e) {
+            string path = OpenFileBrowser();
+            if (path.Length == 0)
+                return;
+            LeftAddress.Text = path;
+
+            leftBRAM = ReadFile(path);
+
+            LeftList.Items.Clear();
+            foreach(BRAMEntry entry in leftBRAM.saves) {
+                LeftList.Items.Add(String.Format("{0} ({1}B)", entry.name, entry.length));
+            }
+
+            LeftFreeSpace.Text = String.Format("Free: {0}B", leftBRAM.freeSpace);
+        }
+
+        private void LeftSave_Click(object sender, EventArgs e) {
+            if (leftBRAM.loaded)
+                SaveFile(leftBRAM, LeftAddress.Text);
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e) {
+            ref BRAM selectedBRAM = ref leftBRAM;
+            ref ListBox selectedList = ref LeftList;
+            if (LeftList.SelectedIndex >= 0) {
+                selectedBRAM = ref leftBRAM;
+                selectedList = ref LeftList;
+            }
+            else if (RightList.SelectedIndex >= 0) {
+                selectedBRAM = ref rightBRAM;
+                selectedList = ref RightList;
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("Please select a file to delete.");
+                return;
+            }
+
+            int index = selectedList.SelectedIndex;
+            selectedBRAM.freeSpace += selectedBRAM.saves[index].length;
+            selectedBRAM.saves.RemoveAt(index);
+            selectedList.Items.RemoveAt(index);
+
+            LeftFreeSpace.Text = String.Format("Free: {0}B", leftBRAM.freeSpace);
+            RightFreeSpace.Text = String.Format("Free: {0}B", rightBRAM.freeSpace);
+
+            if (selectedList.Items.Count > index) {
+                selectedList.SelectedIndex = index;
+            }
+            else {
+                selectedList.SelectedIndex = selectedList.Items.Count - 1;
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e) {
+            if (!(leftBRAM.loaded && rightBRAM.loaded)) {
+                System.Windows.Forms.MessageBox.Show("Please load two BRAM files.");
+                return;
+            }
+
+            ref BRAM selectedBRAM = ref leftBRAM;
+            ref BRAM otherBRAM = ref rightBRAM;
+            ListBox selectedList = new ListBox();
+            ListBox otherList = new ListBox();
+            if (LeftList.SelectedIndex >= 0) {
+                selectedList = LeftList;
+                otherList = RightList;
+            }
+            else if (RightList.SelectedIndex >= 0) {
+                selectedBRAM = ref rightBRAM;
+                otherBRAM = ref leftBRAM;
+                selectedList = RightList;
+                otherList = LeftList;
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("Please select a file to copy.");
+                return;
+            }
+
+            BRAMEntry entry = selectedBRAM.saves[selectedList.SelectedIndex];
+            if (otherBRAM.freeSpace < entry.length) {
+                System.Windows.Forms.MessageBox.Show("Not enough space in destination BRAM.");
+                return;
+            }
+            otherBRAM.saves.Add(entry);
+            otherList.Items.Add(String.Format("{0} ({1}B)", entry.name, entry.length));
+            otherBRAM.freeSpace -= entry.length;
+
+            LeftFreeSpace.Text = String.Format("Free: {0}B", leftBRAM.freeSpace);
+            RightFreeSpace.Text = String.Format("Free: {0}B", rightBRAM.freeSpace);
+        }
+
+        private void UpButton_Click(object sender, EventArgs e) {
+            BRAM selectedBRAM = new BRAM();
+            ListBox selectedList = new ListBox();
+            if (LeftList.SelectedIndex >= 0) {
+                selectedBRAM = leftBRAM;
+                selectedList = LeftList;
+            }
+            else if (RightList.SelectedIndex >= 0) {
+                selectedBRAM = rightBRAM;
+                selectedList = RightList;
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("Please select a file to move.");
+                return;
+            }
+
+            int index = selectedList.SelectedIndex;
+            if (index > 0) {
+                SwapListItems(selectedBRAM.saves, index, index - 1);
+                SwapListItems(selectedList.Items, index, index - 1);
+                selectedList.SelectedIndex--;
+            }
+        }
+
+        private void DownButton_Click(object sender, EventArgs e) {
+            BRAM selectedBRAM = new BRAM();
+            ListBox selectedList = new ListBox();
+            if (LeftList.SelectedIndex >= 0) {
+                selectedBRAM = leftBRAM;
+                selectedList = LeftList;
+            }
+            else if (RightList.SelectedIndex >= 0) {
+                selectedBRAM = rightBRAM;
+                selectedList = RightList;
+            }
+            else {
+                System.Windows.Forms.MessageBox.Show("Please select a file to move.");
+                return;
+            }
+
+            int index = selectedList.SelectedIndex;
+            if (index < selectedList.Items.Count - 1) {
+                SwapListItems(selectedBRAM.saves, index, index + 1);
+                SwapListItems(selectedList.Items, index, index + 1);
+                selectedList.SelectedIndex++;
+            }
+        }
+
+        private void LeftList_SelectedIndexChanged(object sender, EventArgs e) {
+            if (LeftList.Focused)
+                RightList.ClearSelected();
+        }
+
+        private void RightList_SelectedIndexChanged(object sender, EventArgs e) {
+            if (RightList.Focused)
+                LeftList.ClearSelected();
+        }
+
+        private void RightSave_Click(object sender, EventArgs e) {
+            if (rightBRAM.loaded)
+                SaveFile(rightBRAM, RightAddress.Text);
+        }
+
+        private void RightBrowse_Click(object sender, EventArgs e) {
+            string path = OpenFileBrowser();
+            if (path.Length == 0)
+                return;
+            RightAddress.Text = path;
+
+            rightBRAM = ReadFile(path);
+
+            RightList.Items.Clear();
+            foreach (BRAMEntry entry in rightBRAM.saves) {
+                RightList.Items.Add(String.Format("{0} ({1}B)", entry.name, entry.length));
+            }
+
+            RightFreeSpace.Text = String.Format("Free: {0}B", rightBRAM.freeSpace);
+        }
+    }
+}
